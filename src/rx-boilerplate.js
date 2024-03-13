ca71de16def3cb581bc912b15a0913c4516c634b
Matt Whalley <matt@mattwhalley.com>
2024-03-13 18:47:22 +0000

    feat: Add boilerplates for components and stories

---
+++ src/rx-boilerplate.js
@@ -0,0 +1,41 @@
+// Import Lit and html
+import { LitElement, html, css } from "lit";
+// TODO: add imports here
+
+// TODO: rename this component
+class RxComponent extends LitElement {
+  static styles = css`
+    :host {
+      display: block;
+    }
+    // TODO: add styles here
+  `;
+
+  // TODO: uncomment if delegatesFocus is needed
+  //   static shadowRootOptions = {
+  //     ...LitElement.shadowRootOptions,
+  //     delegatesFocus: true,
+  //   };
+
+  static properties = {
+    // TODO: add properties here
+  };
+
+  constructor() {
+    super();
+    // TODO: add properties here
+  }
+
+  render() {
+    return html`
+      // TODO: add component markup here
+      <div>
+        <slot></slot>
+      </div>
+    `;
+  }
+}
+
+// Define the new element
+// TODO: define the new element
+customElements.define("rx-component", RxComponent);
